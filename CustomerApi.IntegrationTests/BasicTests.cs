ccafe2d8b4b68152a4392dad40d9085e2a83fb20
abhishek lal <abhisheklal04@gmail.com>
2019-06-02 22:53:39 +1000

    added more integration tests

--- CustomerApi.IntegrationTests/BasicTests.cs
+++ CustomerApi.IntegrationTests/BasicTests.cs
@@ -13,7 +13,10 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
 using CustomerApi.Models.Response;
+using CustomerApi.Models.Request;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Text;
 
 namespace IntegrationTests
 {
@@ -32,6 +35,9 @@ namespace IntegrationTests
             {
                 AllowAutoRedirect = false
             });
+
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
         [Fact]
@@ -41,13 +47,99 @@ namespace IntegrationTests
             var response = await _client.GetAsync("v1/Customer?isArchived=false&pageNumber=1&pageSize=20&sortCol=Id&sortDesc=false");
 
             Assert.True(response.StatusCode == HttpStatusCode.OK);
-            
+
             if (response.Content != null)
             {
                 var customerResponse = JsonConvert.DeserializeObject<CustomerListResponse>(
                     response.Content.ReadAsStringAsync().Result);
                 Assert.NotEmpty(customerResponse.Items);
             }
+        }
+
+        [Fact]
+        public async Task Get_SingleCustomerDetails()
+        {
+            var responseAllCustomers = await _client.GetAsync("v1/Customer?isArchived=false&pageNumber=1&pageSize=20&sortCol=Id&sortDesc=false");
+
+            var customerAllResponse = JsonConvert.DeserializeObject<CustomerListResponse>(
+                    responseAllCustomers.Content.ReadAsStringAsync().Result);
+            var singleCustomer = customerAllResponse.Items[0];
+
+            // Arrange
+            var response = await _client.GetAsync("v1/Customer/" + singleCustomer.Id);
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+
+            if (response.Content != null)
+            {
+                var customerResponse = JsonConvert.DeserializeObject<CustomerResponse>(
+                    response.Content.ReadAsStringAsync().Result);
+
+                Assert.NotNull(customerResponse);
+                Assert.NotNull(customerResponse.Id.ToString());
+                Assert.NotNull(customerResponse.FirstName.ToString());
+                Assert.NotNull(customerResponse.LastName.ToString());
+            }
+        }
+
+        [Fact]
+        public async Task Delete_a_Customer()
+        {
+            var responseAllCustomers = await _client.GetAsync("v1/Customer?isArchived=false&pageNumber=1&pageSize=20&sortCol=Id&sortDesc=false");
+
+            var customerAllResponse = JsonConvert.DeserializeObject<CustomerListResponse>(
+                    responseAllCustomers.Content.ReadAsStringAsync().Result);
+            var singleCustomer = customerAllResponse.Items[0];
+
+            // Arrange
+            var response = await _client.DeleteAsync("v1/Customer/" + singleCustomer.Id);
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+
+        }
+
+        [Fact]
+        public async Task Update_a_Customer()
+        {
+            var responseAllCustomers = await _client.GetAsync("v1/Customer?isArchived=false&pageNumber=1&pageSize=20&sortCol=Id&sortDesc=false");
+
+            var customerAllResponse = JsonConvert.DeserializeObject<CustomerListResponse>(
+                    responseAllCustomers.Content.ReadAsStringAsync().Result);
+            var singleCustomer = customerAllResponse.Items[0];
+
+            var updateRequest = new CustomerUpdateRequest()
+            {
+                FirstName = singleCustomer.FirstName,
+                LastName = singleCustomer.LastName,
+                DateOfBirth = singleCustomer.DateOfBirth
+            };
+
+            var jsonString = JsonConvert.SerializeObject(updateRequest);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var response = await _client.PutAsync("v1/Customer/" + singleCustomer.Id, content);
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+
+        }
+
+        [Fact]
+        public async Task Add_a_Customer()
+        {
+            var updateRequest = new CustomerUpdateRequest()
+            {
+                FirstName = "New",
+                LastName = "singleCustomer",
+                DateOfBirth = DateTime.Now.Date
+            };
+
+            var jsonString = JsonConvert.SerializeObject(updateRequest);
+            var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+            var response = await _client.PostAsync("v1/Customer/", content);
+
+            Assert.True(response.StatusCode == HttpStatusCode.OK);
+
         }
 
     }
